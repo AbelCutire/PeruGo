46289bb7fdb77f9fbd9657c928a9dbe850fdc5f8
eduardomendozamaque <eduardo,mendoza.maque@ucsp.edu.pe>
2025-11-19 19:56:43 -0500

    Update layout

--- PeruGo/src/app/layout.js
+++ PeruGo/src/app/layout.js
@@ -24,7 +24,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        />
+      </head>
+
       <body>{children}</body>
     </html>
   );
 }
+
