b415a1347ce719f9a44134acfcc98d7a02bdb3c4
eduardomendozamaque <eduardo.mendoza.maque@ucsp.edu.pe>
2025-12-09 19:59:34 -0500

    Update page.jsx

--- PeruGo/src/app/explorar/page.jsx
+++ PeruGo/src/app/explorar/page.jsx
@@ -1,8 +1,13 @@
 import SectionExplorar from '@/components/SectionExplorar';
 
-export default function Page() {
+export const metadata = {
+  title: "Explorar Destinos | PeruGo",
+};
+
+export default function ExplorarPage() {
   return (
     <main>
+      {/* El Header viene del layout global */}
       <SectionExplorar />
     </main>
   );
